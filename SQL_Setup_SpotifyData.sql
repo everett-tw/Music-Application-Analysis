740c1af0815f671e82725a5a7bff1ba6b330a1fb
AmeliaC <82566669+acorea@users.noreply.github.com>
2021-09-15 19:26:47 -0700

    SQL Query and load

---
+++ SQL_Setup_SpotifyData.sql
@@ -0,0 +1,13 @@
+CREATE TABLE musicSQL (
+	artist VARCHAR(150),
+	title VARCHAR(150),
+	chart VARCHAR (50),
+	rank INT,
+	date DATE,
+	region VARCHAR (50),
+	trend VARCHAR (50),
+	streams INT
+);
+
+SELECT *
+From musicSQL
